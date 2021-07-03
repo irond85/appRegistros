5f8965401a94adea9d542552a9373b068ebb6e90
irond85 <ofocacion.34@gmail.com>
2021-07-02 21:16:50 -0500

    Antes de las gráficas xd

--- app/src/main/java/com/example/appregistros/LoginActivity.java
+++ app/src/main/java/com/example/appregistros/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.appregistros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Patterns;
@@ -97,35 +99,38 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     //Métodos que serán llamados en algún momento
-    public void RecuperarAct(){
+    public void RecuperarAct() {
         //Metodo para cambiar entre activity
         Intent Recuperar = new Intent(this,RecuperarActivity.class);
         startActivity(Recuperar);
         finish();
     }
     
-    public void RegistrarAct(){
+    public void RegistrarAct() {
         Intent Registro = new Intent(this,RegistrarActivity.class);
         startActivity(Registro);
         finish();
     }
 
-    public void Ingresar(){
+    public void Ingresar() {
         mail = txtMail.getText().toString();
         password = txtPass.getText().toString();
-       if(!ValidarEmail(mail) || password.equals("")){
+       if(!ValidarEmail(mail) || password.equals("")) {
            if (!ValidarEmail(mail)){
                txtMail.setError("Email no válido");
            }
-           if(password.equals("")){
+           if(password.equals("")) {
                txtPass.setError("Por favor introduce una contraseña");
            }
        }else {
            //Acá va la validación con la base de datos o el firebase Auth para entrar a la app
            /*if(mail.equals("admin@admin.com") && password.equals("1234")){
-               Toast.makeText(this, "Bienvenido Iron", Toast.LENGTH_SHORT).show();
-               */if(!cboxRecordarDatos.isChecked()){
+               Toast.makeText(this, "Bienvenido Iron", Toast.LENGTH_SHORT).show();*/
+               if(!cboxRecordarDatos.isChecked()) {
                    LimpiarLogin();
+               }else if(cboxRecordarDatos.isChecked()){
+                   RecordarDatos();
+                   Toast.makeText(this, "Entra al if del recordar ok", Toast.LENGTH_SHORT).show();
                }
                MainAct();
            /*}else {
@@ -134,13 +139,13 @@ public class LoginActivity extends AppCompatActivity {
        }
     }
 
-    public void MainAct(){
+    public void MainAct() {
         Intent Main = new Intent(LoginActivity.this,MainActivity.class);
         startActivity(Main);
         finish();
     }
 
-    public void LimpiarLogin(){
+    public void LimpiarLogin() {
         txtMail.setText("");
         txtPass.setText("");
     }
@@ -150,4 +155,28 @@ public class LoginActivity extends AppCompatActivity {
         return pattern.matcher(mail).matches();
     }
 
+    public void RecordarDatos() {
+        //RECORDAR DATOS CON SHAREDPREFERENCES PARA GUARDARLO EN EL DISPOSITIVO
+        SharedPreferences recordarDatos = getSharedPreferences("DatosUsuarios", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = recordarDatos.edit();
+
+        String mail = txtMail.getText().toString();
+        String password = txtPass.getText().toString();
+
+        editor.putString("Correo", mail);
+        editor.putString("Password", password);
+
+        editor.commit();
+        Toast.makeText(this, "Se han guardado los datos", Toast.LENGTH_SHORT).show();
+    }
+
+    public void MostrarDatos() {
+        SharedPreferences recordarDatos = getSharedPreferences("DatosUsuarios", Context.MODE_PRIVATE);
+
+        String correoRecordado = recordarDatos.getString("Correo", "");
+        String passRecordado = recordarDatos.getString("Password", "");
+
+        txtMail.setText(correoRecordado);
+        txtPass.setText(passRecordado);
+    }
 }
--- app/src/main/java/com/example/appregistros/MainActivity.java
+++ app/src/main/java/com/example/appregistros/MainActivity.java
@@ -1,35 +1,55 @@
 package com.example.appregistros;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtBienvenido;
+    SwipeRefreshLayout swpActualizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtBienvenido = findViewById(R.id.txtBienvenida);
+        swpActualizar = findViewById(R.id.swpActualizar);
+
+        //METODO PARA ACTUALIZAR LA TABLA PERO EN EL MOMENTO MUESTRA UN SNACKBAR XD
+        swpActualizar.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //ACA AGREGAMOS EL METODO OBVIA XD
+                Actualizar();
+            }
+        });
 
     }
 
+    //CAMBIAR DE ACTS Y ELIMINAR THIS
     public void LoginAct(){
         Intent Login = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(Login);
         finish();
     }
 
+    //METODO PARA REGRESAR
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             LoginAct();
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    public void Actualizar() {
+        //SERIA BUENO AGREGAR UN SNACKBAR PARA MOSTRAR UN MENSAJE DE ACTUALIZADO O ALGO SIMILAR
+        Toast.makeText(this, "Se está actualizando eweeeeeeee", Toast.LENGTH_SHORT).show();
+        swpActualizar.setRefreshing(false);
+        Toast.makeText(this, "Atualizaooo sociooo", Toast.LENGTH_SHORT).show();
     }
 }
